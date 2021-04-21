07a150af85d9c2ba4d612e617a7c413034bf1d47
Robert <robert@voltronwork.com>
2021-04-21 16:50:09 +0300

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+###Proxy checker
+
+####Install
+1. `composer install`
+2. Download GeoLite2-City.mmdb from https://dev.maxmind.com/geoip/geoip2/geolite2/
+3. Create .env file
+
+Run the command: `php application.php proxy:check 104.236.123.137:8080`
+
+Output info:
+```
+Info 104.236.123.137:8080
+Speed: 397.000000
+City: United States/Clifton
+Real IP: 10.0.10.22:44736
+Check time: 0.551576
+```
--- src/App/Command/CheckCommand.php
+++ src/App/Command/CheckCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Command;
 
+use GeoIp2\Database\Reader;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,26 +14,113 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class CheckCommand extends Command
 {
+    /**
+     * @var string
+     */
     protected static $defaultName = 'proxy:check';
 
+    /**
+     * @inheritDoc
+     */
     protected function configure()
     {
-        // ...
+        $this->addArgument('ip', InputArgument::REQUIRED, 'Which Ip should you check?');
     }
 
+    private $geoLiteDBPath;
+
+    public function __construct($geoLiteDBPath)
+    {
+        parent::__construct();
+        $this->geoLiteDBPath = $geoLiteDBPath;
+    }
+
+    /**
+     * @inheritDoc
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ... put here the code to create the user
+        $content = 'Info ' . $input->getArgument('ip') . "\n";
+        $proxies = [$input->getArgument('ip')];
+        foreach ($this->check($proxies, 'http') as $result) {
+            $content .= sprintf(
+                "Speed: %f\nCity: %s\nReal IP: %s\nCheck time: %f",
+                $result['speed'],
+                $result['city'],
+                $result['real_ip'],
+                $result['check_time']
+            );
+        }
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
+        $output->writeln($content);
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
         return Command::SUCCESS;
+    }
 
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
+    /**
+     * @param array $proxies
+     * @param string $type
+     * @return array
+     */
+    protected function check(array $proxies, string $type)
+    {
+        $mc = curl_multi_init();
+        for ($thread_no = 0; $thread_no < count($proxies); $thread_no++) {
+            $c[$thread_no] = curl_init();
+            curl_setopt($c[$thread_no], CURLOPT_URL, "http://google.com");
+            curl_setopt($c[$thread_no], CURLOPT_HEADER, 0);
+            curl_setopt($c[$thread_no], CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($c[$thread_no], CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($c[$thread_no], CURLOPT_TIMEOUT, 10);
+            curl_setopt($c[$thread_no], CURLOPT_PROXY, trim($proxies [$thread_no]));
+            curl_setopt($c[$thread_no], CURLOPT_PROXYTYPE, $type);
+            curl_multi_add_handle($mc, $c[$thread_no]);
+        }
+
+        $success = [];
+        do {
+            while (($execrun = curl_multi_exec($mc, $running)) == CURLM_CALL_MULTI_PERFORM) {
+            }
+            if ($execrun != CURLM_OK) {
+                break;
+            }
+            while ($done = curl_multi_info_read($mc)) {
+                $info = curl_getinfo($done['handle']);
+                $ip = trim($proxies [array_search($done['handle'], $c)]);
+                if (301 === $info['http_code']) {
+                    $success[] = [
+                        'ip' => $ip,
+                        'speed' => $info['speed_download'],
+                        'city' => $this->getCountryCity($ip),
+                        'real_ip' => $info['local_ip'] . ':' . $info['local_port'],
+                        'check_time' => $info['total_time'],
+                    ];
+                } else {
+                    // TODO: Invalid proxy IP
+                }
+                curl_multi_remove_handle($mc, $done ['handle']);
+            }
+        } while ($running);
+        curl_multi_close($mc);
+
+        return $success;
+    }
+
+    /**
+     * @param string $ip
+     * @return string
+     */
+    private function getCountryCity(string $ip): string
+    {
+        $ipPort = explode(':', $ip);
+        $ipForCountry = reset($ipPort);
+
+        try {
+            $reader = new Reader($this->geoLiteDBPath);
+            $record = $reader->city($ipForCountry);
+            return $record->country->name . '/' . $record->city->name;
+        } catch (\Throwable $throwable) {
+            return '/';
+        }
     }
 }
